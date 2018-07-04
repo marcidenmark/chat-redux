e3d1dedf82f11cff4c652c077cb5a3c4c2ded645
marci papineau <marcidenmark@hotmail.com>
2018-07-04 12:16:43 +0200

    added redux-logger and redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise'
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+//Marci, do to:
+//import each reducer from './reducers/reducer_name';
+//import each reducer from './reducers/reducer_name';
+//import each reducer from './reducers/reducer_name';
+//import each reducer from './reducers/reducer_name';
+//import each reducer from './reducers/reducer_name';
 
 // State and reducers
 const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
