184a7d689b33ece5df319a3cfdebdc2e0c0c97f5
marci papineau <marcidenmark@hotmail.com>
2018-07-10 14:41:22 +0200

    different way to write the text in message component. fpo only.

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -6,14 +6,11 @@ class Message extends Component {
 	render() {
 		return (
 			<div>
-				<h6 className="bold">Author 1 Name
+				<div className="author">Author 1 Name
 				<span className="lite"> time </span>
-				</h6>
-				<h6>Author's message text will be here.</h6>
-				<h6 className="bold">Author 2 Name
-				<span className="lite"> time </span>
-				</h6>
-				<h6>Author's message text will be here.</h6>
+				</div>
+				<div className="author-message">Author's message text will be here.</div>
+
 
 			</div>
 		)
