b86a350c721470ed3cb9781e14b7a144e7630e44
marci papineau <marcidenmark@hotmail.com>
2018-07-10 14:25:39 +0200

    stylesheets for messages

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -6,7 +6,14 @@ class Message extends Component {
 	render() {
 		return (
 			<div>
-			<h6> fjkdsæafjadsæ</h6>
+				<h6 className="bold">Author 1 Name
+				<span className="lite"> time </span>
+				</h6>
+				<h6>Author's message text will be here.</h6>
+				<h6 className="bold">Author 2 Name
+				<span className="lite"> time </span>
+				</h6>
+				<h6>Author's message text will be here.</h6>
 
 			</div>
 		)
